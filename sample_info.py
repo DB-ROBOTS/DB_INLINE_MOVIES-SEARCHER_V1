73e3ead3c1c397b8567edc953bdedfc854700500
DB ROBOTS <85598210+DB-ROBOTS@users.noreply.github.com>
2021-07-31 07:36:47 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -22,10 +22,31 @@ DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
-START_MSG = """
-**Hi, I'm Media Search bot**
+START_MSG = """👋☺️ʜɪ Hᴇʟʟᴏ ,
 
-Here you can search files in inline mode. Just press follwing buttons and start searching.
+🔎 I Aᴍ  𝗗𝗕 𝗠𝗢𝗩𝗜𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗘𝗔𝗥𝗖𝗛 𝗕𝗢𝗧 🧐🕵️
+
+Hᴇʀᴇ Yᴏᴜ Cᴀɴ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇs Fɪʟᴇs Iɴ Iɴʟɪɴᴇ Mᴏᴅᴇ , Aɴᴅ Aʟsᴏᴏ I Aᴍ Pʀᴏᴠɪᴅᴇ Yᴏᴜ Sᴀᴍᴇ As Aᴜᴛᴏ Fɪʟᴛᴇʀs Iɴ Gʀᴏᴜᴘ Aɴᴅ Pʀɪᴠᴀᴛᴇ Mᴇssᴀɢᴇ Aʟsᴏᴏ ,🤩
+
+🤔𝐇𝐨𝐰 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐞 𝐛𝐨𝐭 !?🤯
+
+🔘Pʀᴇss Tʜᴇ [ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇs...] Bᴜᴛᴛᴏɴ ,
+
+🔸ᴀɴᴅ Eɴᴛᴇʀ Uʀs Mᴏᴠɪᴇ Nᴀᴍᴇ Oʀ Mᴇᴅɪᴀ Nᴀᴍᴇ 
+
+📝𝗡𝗼𝘁𝗲 :- Oɴʟʏ Eɴᴛᴇʀ Cᴏʀʀᴇᴄᴛ Sᴘᴇʟʟɪɴɢs { Sᴀᴍᴇ Iɴ Gʀᴏᴜᴘ Aʟsᴏᴏ }
+
+🔎𝗦𝗲𝗮𝗿𝗰𝗵 𝗙𝗼𝗿𝗺𝗮𝘁𝗲 :- [ Mᴏᴠɪᴇ Nᴀᴍᴇ ]( Mᴏᴠɪᴇ Yᴇᴀʀ )✔️
+
+ᴏʀ Usᴇ  @imdb Lɪɴᴋ Tᴏ Pᴇʀғᴇᴄᴛ Mᴏᴠɪᴇ Sᴘᴇʟʟɪɴɢ.🏷️
+
+🕵️ᴀɴᴅ Aғᴛᴇʀ Yᴏᴜ Gᴏᴛ Tʜᴇ Lɪɴᴋs Pʀᴇss Aɴʏ Oɴᴇ Bᴜᴛᴛᴏɴ, 
+
+🧐ᴀɴᴅ Pʀᴇss Sᴛᴀʀᴛ Bᴜᴛᴛᴏɴ, Aɴᴅ Eɴᴊᴏʏ Tʜᴇ Mᴏᴠɪᴇ Fɪʟᴇ.🤩 
+
+🤖𝐁𝐎𝐓𝐬 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 :- @DB_ROBOTS📡
+
+🆕𝐀𝐍𝐘 𝐈𝐒𝐒𝐔𝐄𝐒 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 :- @DB_HELPER.
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
